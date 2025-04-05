306218a3e90475e33162624d1acce892354a8102
Brian <brianhenriquedp@gmail.com>
2025-04-04 21:51:20 -0400

    Exemplo 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 numeros_pares = [2,4,6,12]
-print(numeros_pares)
+print(numeros_pares[2])
 
-# A lista permite armazenar varios dados de uma vez, como se fosse uma caixa com varios compartimentos, onde cada compartimento tem um numero de identificacao, o indice.
+# Ao colocar um indice [2] em numeros_pares na função print, nós dizemos para exibir o terceiro elemento da lista, que é o número 6.
+# O indice começa em 0, então o primeiro elemento é 0, o segundo é 1 e assim por diante.
