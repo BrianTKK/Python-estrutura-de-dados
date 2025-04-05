97996d93b5126e83f6685d6e70fc9f418828ecb8
Brian <brianhenriquedp@gmail.com>
2025-04-04 21:59:34 -0400

    Exemplo 4

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ numeros_pares = [2,4,6,12]
 
 numeros_pares.append(80)
 
+numeros_pares.remove(4)
+
 print(numeros_pares)
 
-# Com o comando da linha 3, adicionamos mais um item dentro da lista, que é o número 80.
+# o comando da quinta linha, .remove, remove o primeiro elemento que for igual ao valor passado como argumento, no caso o 4.
