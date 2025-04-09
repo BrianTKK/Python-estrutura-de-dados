cf2ea184c2a1908600cf1966585b9bee5422b35c
Brian <brianhenriquedp@gmail.com>
2025-04-09 16:45:40 -0400

    Exemplo 10 map ou dicionario

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-numeros = set()
+pessoa = {
+    "nome": "Joao"
+}
 
-numeros.add(1)
-numeros.add(2)
-numeros.add(3)
+print(pessoa)
 
-print(numeros)
-
-# Set também é um tipo de lista mas não é ordenada(não pode ser acessada por indice) e não aceita valores duplicados.
+# Estrutura dicionario ou map é uma coleção de pares chave-valor.
